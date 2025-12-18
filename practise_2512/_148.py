7dadfc8f3d9fff095410b22c46ce78b3a8ac41ff
Neal Chan <nealchan@buaa.edu.cn>
2025-12-18 21:01:24 +0800

    a

--- practise_2512/_148.py
+++ practise_2512/_148.py
@@ -8,26 +8,89 @@ class ListNode:
         self.val = val
         self.next = None
 
+
 class Solution:
     def sortList(self, head):
         """
-        快排
+        归并排序
         """
+        if not head or not head.next:
+            return head
+
+        mid = self.find_middle(head)
+        sec = mid.next
+        mid.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(sec)
+
+        return self.merge_two_sorted_lists(left, right)
+
+    def find_middle(self, head):
+        """找到链表的中点"""
         if not head:
             return
 
-        lst = []
-        while head:
-            lst.append(head.val)
-            head = head.next
+        s, f = head, head.next
+        while f and f.next:
+            s = s.next
+            f = f.next.next
 
-        lst.sort()
+        return s
+
+    def merge_two_sorted_lists(self, list1, list2):
+        """合并两个lst"""
+        if not list1 and not list2:
+            return
 
         dummy = ListNode(-1)
-        p = dummy
-        for i in range(len(lst)):
-            p.next = ListNode(lst[i])
+        p, p1, p2 = dummy, list1, list2
 
-        p.next = None
+        while p1 and p2:
+            if p1.val <= p2.val:
+                p.next = p1
+                p1 = p1.next
+            else:
+                p.next = p2
+                p2 = p2.next
+
+            p = p.next
+
+        if p1:
+            p.next = p1
+
+        if p2:
+            p.next = p2
 
         return dummy.next
+
+
+def create_from_lst(lst):
+    dummy = ListNode(-1)
+    p = dummy
+
+    for l in lst:
+        p.next = ListNode(l)
+        p = p.next
+
+    return dummy.next
+
+
+def print_linked_list(head):
+    while head:
+        print(head.val)
+        head = head.next
+
+
+if __name__ == '__main__':
+    lst = [4, 2, 1, 3]
+    llst = create_from_lst(lst)
+    print_linked_list(llst)
+    print('=' * 10)
+
+    a = Solution()
+    res = a.sortList(llst)
+    print_linked_list(res)
+
+
+
